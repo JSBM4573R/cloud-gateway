02e03c5e69a8889f14794356e273d61c6722c836
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-11 01:56:03 -0500

    Se agrega la dependencia de Hystrix y se habilita en el CloudGatewayApplication

--- src/main/java/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/cloudgateway/CloudGatewayApplication.java
@@ -2,10 +2,13 @@ package cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
