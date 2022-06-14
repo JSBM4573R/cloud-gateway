e8b27d86f775bb7acb3e0fa3957770be395c87b9
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-13 22:33:24 -0500

    Se crea el controlador FallBackMethodController el cual contiene dos metodos que mapean a un path para user-service y department-service en caso de que alguno de esos servicios se fallen o se genere un timeout. Adicionalmente se modifica el application.yml agregando los filters CircuitBreaker para los dos servicios con su fallbackurl al path creado anteriormente

---
+++ src/main/java/cloudgateway/FallBackMethodController.java
@@ -0,0 +1,20 @@
+package cloudgateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try Again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department Service is taking longer than Expected." +
+                " Please try Again later";
+    }
+}
